f648bd937b2d5d73095f523caef2ea5365c82320
Zhongpeng Lin (林中鹏) <lin.zhp@gmail.com>
2013-07-07 11:33:25 -0700

    Indexed ASTNodes

---
+++ src/edu/ucsc/cs/simulation/Indexer.java
@@ -0,0 +1,70 @@
+package edu.ucsc.cs.simulation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.eclipse.jdt.internal.compiler.ASTVisitor;
+import org.eclipse.jdt.internal.compiler.ast.ASTNode;
+import org.eclipse.jdt.internal.compiler.ast.FieldDeclaration;
+import org.eclipse.jdt.internal.compiler.ast.MethodDeclaration;
+import org.eclipse.jdt.internal.compiler.ast.TypeDeclaration;
+import org.eclipse.jdt.internal.compiler.lookup.BlockScope;
+import org.eclipse.jdt.internal.compiler.lookup.ClassScope;
+import org.eclipse.jdt.internal.compiler.lookup.CompilationUnitScope;
+import org.eclipse.jdt.internal.compiler.lookup.MethodScope;
+
+import ch.uzh.ifi.seal.changedistiller.ast.java.JavaASTNodeTypeConverter;
+import ch.uzh.ifi.seal.changedistiller.model.classifiers.EntityType;
+import edu.ucsc.cs.utils.LogManager;
+
+public class Indexer extends ASTVisitor {
+	public HashMap<String, ArrayList<ASTNode>> nodeIndex = new HashMap<String, ArrayList<ASTNode>>();
+	private JavaASTNodeTypeConverter converter = new JavaASTNodeTypeConverter();
+
+	
+	private void index(ASTNode node) {
+		EntityType type = converter.convertNode(node);
+		if (type != null) {
+			String typeString = type.toString();
+			if (nodeIndex.containsKey(typeString)) {
+				nodeIndex.get(typeString).add(node);
+			} else {
+				nodeIndex.put(typeString, new ArrayList<ASTNode>(Arrays.asList(node)));
+			}
+		} else {
+			LogManager.getLogger().info("Invalid ASTNode class: " + node.getClass());
+		}
+	}
+
+	@Override
+	public boolean visit(TypeDeclaration td, ClassScope scope) {
+		index(td);
+		return true;
+	}
+
+	@Override
+	public boolean visit(TypeDeclaration localTypeDeclaration, BlockScope scope) {
+		index(localTypeDeclaration);
+		return true;
+	}
+
+	@Override
+	public boolean visit(TypeDeclaration typeDeclaration,
+			CompilationUnitScope scope) {
+		index(typeDeclaration);
+		return true;
+	}
+	
+	@Override
+	public boolean visit(MethodDeclaration method, ClassScope scope) {
+		index(method);
+		return true;
+	}
+	
+	@Override
+	public boolean visit(FieldDeclaration field, MethodScope scope) {
+		index(field);
+		return true;
+	}
+}
--- src/edu/ucsc/cs/simulation/Simulator.java
+++ src/edu/ucsc/cs/simulation/Simulator.java
@@ -4,88 +4,30 @@ import java.io.File;
 import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 import org.eclipse.jdt.internal.compiler.ast.ASTNode;
+import org.eclipse.jdt.internal.compiler.ast.CompilationUnitDeclaration;
 
 import static java.lang.System.out;
 
 
 import ch.uzh.ifi.seal.changedistiller.ast.ASTHelper;
-import ch.uzh.ifi.seal.changedistiller.ast.java.JavaASTNodeTypeConverter;
-import ch.uzh.ifi.seal.changedistiller.model.classifiers.EntityType;
 import ch.uzh.ifi.seal.changedistiller.structuredifferencing.java.JavaStructureNode;
-import ch.uzh.ifi.seal.changedistiller.treedifferencing.Node;
-
-import edu.ucsc.cs.utils.LogManager;
 
 public class Simulator {
-	private HashMap<String, ArrayList<JavaStructureNode>> nodeIndex;
 	private ASTHelper<JavaStructureNode> astHelper;
+	private HashMap<String, ArrayList<ASTNode>> nodeIndex;
 	
 	public Simulator() throws SQLException {
-		nodeIndex = new HashMap<String, ArrayList<JavaStructureNode>>();
 		JavaParser parser = new JavaParser();
 		astHelper = parser.getASTHelper(new File("TextArea.java"));
 		JavaStructureNode tree = astHelper.createStructureTree();
-		ASTNode classNode = tree.getChildren().get(0).getASTNode();
-		ClassModifier modifier = new ClassModifier(classNode);
-		modifier.addClass();
-		out.print(classNode);
-//		indexNodes(tree);
-	}
-	
-	/**
-	 * Traverse the AST using pre-order depth first search to index all tree nodes by their types
-	 * @param root
-	 * @return
-	 */
-	public void indexNodes(JavaStructureNode root) {
-		JavaASTNodeTypeConverter converter = new JavaASTNodeTypeConverter();
-		EntityType type = converter.convertNode(root.getASTNode());
-		if (type != null) {
-			String typeString = type.toString();
-			if (nodeIndex.containsKey(typeString)) {
-				nodeIndex.get(typeString).add(root);
-			} else {
-				nodeIndex.put(typeString, new ArrayList<JavaStructureNode>(Arrays.asList(root)));
-			}
-		} else {
-			LogManager.getLogger().info("Invalid ASTNode class: " + root.getASTNode().getClass());
-		}
-		for (JavaStructureNode child : root.getChildren()) {
-			indexNodes(child);
-		}
-	}
-	
-	private String getString(JavaStructureNode root, int indent) {
-		Node declarationTree = astHelper.createDeclarationTree(root);
-		StringBuilder buffer = new StringBuilder();
-		// Printing declaration
-		EntityType entityType = declarationTree.getEntity().getType();
-		if (entityType != null) {
-			for (int i = 0; i < indent; i++) {
-				buffer.append('\t');
-			}
-			switch (entityType.toString()) {
-			case "CLASS":
-				
-				break;
-			case "METHOD":
-				break;
-			case "FIELD":
-				break;
-			}
-		}
-		for (JavaStructureNode c : root.getChildren()) {
-			buffer.append(getString(c, indent + 1));
-		}
-		return buffer.toString();
-	}
-	
-	public String getString(JavaStructureNode root) {
-		return getString(root, 0);
+		CompilationUnitDeclaration astNode = (CompilationUnitDeclaration)tree.getASTNode();
+		Indexer indexer = new Indexer();
+		astNode.traverse(indexer, astNode.scope);
+		nodeIndex = indexer.nodeIndex;
+		out.println(nodeIndex);
 	}
 
 	/**
